9f9d326aeafa95aeec03e82122da1a2b52672e31
l1ii <97005881+l1ii@users.noreply.github.com>
2022-02-01 19:49:38 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,35 @@
+-- DONG ADMIN | Made by bIue#2958
+
+-- Instances 
+
 local DongAdmin = Instance.new("ScreenGui")
 local Boot = Instance.new("Frame")
 local Title = Instance.new("TextLabel")
 local Description = Instance.new("TextLabel")
-local Continue = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
-local Delete = Instance.new("TextButton")
+local Continue = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
-local UICorner_3 = Instance.new("UICorner")
 local Main = Instance.new("Frame")
 local CmdBar = Instance.new("Frame")
-local UICorner_4 = Instance.new("UICorner")
+local UICorner_3 = Instance.new("UICorner")
 local TextBox = Instance.new("TextBox")
+local Color = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local CmdList = Instance.new("Frame")
+local Title_2 = Instance.new("TextLabel")
+local UICorner_5 = Instance.new("UICorner")
+local ScrollingFrame = Instance.new("ScrollingFrame")
+local Command = Instance.new("TextLabel")
+local UICorner_6 = Instance.new("UICorner")
+local UIListLayout = Instance.new("UIListLayout")
+local Command_2 = Instance.new("TextLabel")
+local UICorner_7 = Instance.new("UICorner")
+local Command_3 = Instance.new("TextLabel")
+local UICorner_8 = Instance.new("UICorner")
+local Command_4 = Instance.new("TextLabel")
+local UICorner_9 = Instance.new("UICorner")
+local Command_5 = Instance.new("TextLabel")
+local UICorner_10 = Instance.new("UICorner")
 
 DongAdmin.Name = "DongAdmin"
 DongAdmin.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -21,16 +40,15 @@ Boot.Parent = DongAdmin
 Boot.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 Boot.Position = UDim2.new(0.324528277, 0, 0.349693239, 0)
 Boot.Size = UDim2.new(0.349999994, 0, 0.300000012, 0)
-Boot.Visible = false
 
 Title.Name = "Title"
 Title.Parent = Boot
 Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Title.BackgroundTransparency = 1.000
-Title.Position = UDim2.new(0, 0, 0.0867075622, 0)
+Title.Position = UDim2.new(0, 0, 0.123517379, 0)
 Title.Size = UDim2.new(1, 0, 0.200000003, 0)
 Title.Font = Enum.Font.GothamSemibold
-Title.Text = "DONG ADMIN"
+Title.Text = "NOTICE"
 Title.TextColor3 = Color3.fromRGB(255, 255, 255)
 Title.TextScaled = true
 Title.TextSize = 14.000
@@ -40,43 +58,30 @@ Description.Name = "Description"
 Description.Parent = Boot
 Description.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Description.BackgroundTransparency = 1.000
-Description.Position = UDim2.new(0.0996226445, 0, 0.286298573, 0)
+Description.Position = UDim2.new(0.0996226445, 0, 0.327198386, 0)
 Description.Size = UDim2.new(0.800000012, 0, 0.300000012, 0)
 Description.Font = Enum.Font.Gotham
-Description.Text = "Dong admin is a script created by bIue#2958. Note that this script is only useable on Prison Life created by Aesthetical. Join the discord server by using the \"discord\" command"
+Description.Text = "Press ; to open the command bar. This admin script is made by bIue#2958 for Prison Life. Join my discord server with the \"discord\" command"
 Description.TextColor3 = Color3.fromRGB(255, 255, 255)
 Description.TextSize = 15.000
 Description.TextWrapped = true
 Description.TextXAlignment = Enum.TextXAlignment.Left
 
+UICorner.CornerRadius = UDim.new(0, 3)
+UICorner.Parent = Boot
+
 Continue.Name = "Continue"
 Continue.Parent = Boot
 Continue.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-Continue.Position = UDim2.new(0.188679233, 0, 0.678936541, 0)
-Continue.Size = UDim2.new(0.300000012, 0, 0.200000003, 0)
+Continue.Position = UDim2.new(0.298652291, 0, 0.666666567, 0)
+Continue.Size = UDim2.new(0.400000006, 0, 0.200000003, 0)
 Continue.Font = Enum.Font.Gotham
-Continue.Text = "I understand"
+Continue.Text = "Continue"
 Continue.TextColor3 = Color3.fromRGB(255, 255, 255)
 Continue.TextSize = 18.000
 
-UICorner.CornerRadius = UDim.new(0, 3)
-UICorner.Parent = Continue
-
-Delete.Name = "Delete"
-Delete.Parent = Boot
-Delete.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-Delete.Position = UDim2.new(0.510188639, 0, 0.678936541, 0)
-Delete.Size = UDim2.new(0.300000012, 0, 0.200000003, 0)
-Delete.Font = Enum.Font.Gotham
-Delete.Text = "Delete GUI"
-Delete.TextColor3 = Color3.fromRGB(255, 0, 0)
-Delete.TextSize = 18.000
-
 UICorner_2.CornerRadius = UDim.new(0, 3)
-UICorner_2.Parent = Delete
-
-UICorner_3.CornerRadius = UDim.new(0, 3)
-UICorner_3.Parent = Boot
+UICorner_2.Parent = Continue
 
 Main.Name = "Main"
 Main.Parent = DongAdmin
@@ -84,16 +89,15 @@ Main.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 Main.BackgroundTransparency = 1.000
 Main.Position = UDim2.new(-3.53902578e-08, 0, -2.23517418e-08, 0)
 Main.Size = UDim2.new(1, 0, 1, 0)
-Main.Visible = false
 
 CmdBar.Name = "CmdBar"
 CmdBar.Parent = Main
 CmdBar.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-CmdBar.Position = UDim2.new(0.200000003, 0, 0.919018388, 0)
-CmdBar.Size = UDim2.new(0.600000024, 0, 0.0500000007, 0)
+CmdBar.Position = UDim2.new(0.249811321, 0, 0.915337384, 0)
+CmdBar.Size = UDim2.new(0.5, 0, 0.0500000007, 0)
 
-UICorner_4.CornerRadius = UDim.new(0, 3)
-UICorner_4.Parent = CmdBar
+UICorner_3.CornerRadius = UDim.new(0, 3)
+UICorner_3.Parent = CmdBar
 
 TextBox.Parent = CmdBar
 TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -108,14 +112,134 @@ TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.TextSize = 14.000
 TextBox.TextXAlignment = Enum.TextXAlignment.Left
 
+Color.Name = "Color"
+Color.Parent = CmdBar
+Color.BackgroundColor3 = Color3.fromRGB(0, 85, 255)
+Color.Position = UDim2.new(0, 0, 0.899999976, 0)
+Color.Size = UDim2.new(1, 0, 0.100000001, 0)
+
+UICorner_4.CornerRadius = UDim.new(0, 3)
+UICorner_4.Parent = Color
+
+CmdList.Name = "CmdList"
+CmdList.Parent = Main
+CmdList.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+CmdList.Position = UDim2.new(0.371320724, 0, 0.374233127, 0)
+CmdList.Size = UDim2.new(0.25, 0, 0.25, 0)
+CmdList.Visible = false
+
+Title_2.Name = "Title"
+Title_2.Parent = CmdList
+Title_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Title_2.BackgroundTransparency = 1.000
+Title_2.Position = UDim2.new(0.147169814, 0, 0.0539877303, 0)
+Title_2.Size = UDim2.new(0.699999988, 0, 0.150000006, 0)
+Title_2.Font = Enum.Font.GothamSemibold
+Title_2.Text = "Commands"
+Title_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title_2.TextScaled = true
+Title_2.TextSize = 30.000
+Title_2.TextWrapped = true
+
+UICorner_5.CornerRadius = UDim.new(0, 3)
+UICorner_5.Parent = CmdList
+
+ScrollingFrame.Parent = CmdList
+ScrollingFrame.Active = true
+ScrollingFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ScrollingFrame.BackgroundTransparency = 1.000
+ScrollingFrame.BorderSizePixel = 0
+ScrollingFrame.Position = UDim2.new(0.0452830195, 0, 0.24539879, 0)
+ScrollingFrame.Size = UDim2.new(0.899999976, 0, 0.699999988, 0)
+ScrollingFrame.CanvasSize = UDim2.new(0, 0, 5, 0)
+ScrollingFrame.ScrollBarThickness = 0
+
+Command.Name = "Command"
+Command.Parent = ScrollingFrame
+Command.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Command.BackgroundTransparency = 1.000
+Command.Size = UDim2.new(1, 0, 0.0250000004, 0)
+Command.Font = Enum.Font.Gotham
+Command.Text = "kill [plr]/[team]/all"
+Command.TextColor3 = Color3.fromRGB(255, 255, 255)
+Command.TextSize = 14.000
+Command.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Command.TextXAlignment = Enum.TextXAlignment.Left
+
+UICorner_6.CornerRadius = UDim.new(0, 3)
+UICorner_6.Parent = Command
+
+UIListLayout.Parent = ScrollingFrame
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+Command_2.Name = "Command"
+Command_2.Parent = ScrollingFrame
+Command_2.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Command_2.BackgroundTransparency = 1.000
+Command_2.Size = UDim2.new(1, 0, 0.0250000004, 0)
+Command_2.Font = Enum.Font.Gotham
+Command_2.Text = "team [team]"
+Command_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Command_2.TextSize = 14.000
+Command_2.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Command_2.TextXAlignment = Enum.TextXAlignment.Left
+
+UICorner_7.CornerRadius = UDim.new(0, 3)
+UICorner_7.Parent = Command_2
+
+Command_3.Name = "Command"
+Command_3.Parent = ScrollingFrame
+Command_3.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Command_3.BackgroundTransparency = 1.000
+Command_3.Size = UDim2.new(1, 0, 0.0250000004, 0)
+Command_3.Font = Enum.Font.Gotham
+Command_3.Text = "bring [plr]/[team]/all"
+Command_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+Command_3.TextSize = 14.000
+Command_3.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Command_3.TextXAlignment = Enum.TextXAlignment.Left
+
+UICorner_8.CornerRadius = UDim.new(0, 3)
+UICorner_8.Parent = Command_3
+
+Command_4.Name = "Command"
+Command_4.Parent = ScrollingFrame
+Command_4.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Command_4.BackgroundTransparency = 1.000
+Command_4.Size = UDim2.new(1, 0, 0.0250000004, 0)
+Command_4.Font = Enum.Font.Gotham
+Command_4.Text = "punish [plr]/[team]/all"
+Command_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+Command_4.TextSize = 14.000
+Command_4.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Command_4.TextXAlignment = Enum.TextXAlignment.Left
+
+UICorner_9.CornerRadius = UDim.new(0, 3)
+UICorner_9.Parent = Command_4
+
+Command_5.Name = "Command"
+Command_5.Parent = ScrollingFrame
+Command_5.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Command_5.BackgroundTransparency = 1.000
+Command_5.Size = UDim2.new(1, 0, 0.0250000004, 0)
+Command_5.Font = Enum.Font.Gotham
+Command_5.Text = "give [plr] [item]"
+Command_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+Command_5.TextSize = 14.000
+Command_5.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Command_5.TextXAlignment = Enum.TextXAlignment.Left
+
+UICorner_10.CornerRadius = UDim.new(0, 3)
+UICorner_10.Parent = Command_5
+
 -- Modules
 
 local modules = {}
 
-do -- Commands
+do
 	local script = Instance.new('ModuleScript', CmdBar)
 	script.Name = "Commands"
-	local function module_script()
+	local function Commands()
 		local Players = game:GetService("Players")
 		local Player = Players.LocalPlayer
 		
@@ -168,7 +292,7 @@ do -- Commands
 		
 		return cmds
 	end
-	modules[script] = module_script
+	modules[script] = Commands()
 end
 
 -- Scripts
@@ -177,7 +301,7 @@ function CommandBarHandler()
 	local script = Instance.new('LocalScript', CmdBar)
 	local req = require
 	local require = function(obj)
-		local fake = fake_module_scripts[obj]
+		local fake = modules[obj]
 		if fake then
 			return fake()
 		end
@@ -217,14 +341,6 @@ coroutine.wrap(CommandBarHandler)()
 
 function OnExecute()
 	local script = Instance.new('LocalScript', DongAdmin)
-	local req = require
-	local require = function(obj)
-		local fake = fake_module_scripts[obj]
-		if fake then
-			return fake()
-		end
-		return req(obj)
-	end
 
 	local gui = script.Parent
 	local Boot = gui:WaitForChild("Boot")
@@ -238,9 +354,6 @@ function OnExecute()
 		Main.Visible = true
 	end)
 	
-	Boot.Delete.Activated:Connect(function()
-		gui:Destroy()
-	end)
 end
 
 coroutine.wrap(OnExecute)()
